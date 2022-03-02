ab6b991eb95d281e5413e3840259e62f4011fac8
Gauri200 <98091595+Gauri200@users.noreply.github.com>
2022-03-02 19:24:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("open_console");
+var first_name = "Gauri";
+var last_name = "Girigosavi";
+
+var a = 10;
+var b = 5;
+
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+
+var add = a+b;
+console.log(add)
+
+var sub = a-b;
+console.log(sub);
+
+var multiplication = a*b;
+console.log(multiplication);
+
+var division = a/b;
+console.log(division);
